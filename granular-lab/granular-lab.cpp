f5c9bf92478f52ae82ad61283ff1dad84d8fb206
Aeziren <miguelsl2000@gmail.com>
2025-05-14 23:46:11 +0100

    work in progress

--- granular-lab/granular-lab.cpp
+++ granular-lab/granular-lab.cpp
@@ -1,3 +1,4 @@
+#include <utility>
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -9,78 +10,18 @@ const int SCREEN_WIDTH{ 640 };
 const int SCREEN_HEIGHT{ 360 };
 const int SCALING{ 2 };
 
-bool MATRIX[640][360] = {};
-
-class World {
-private:
-	Particle* matrix[640][360] = {};
-
-public:
-	World() {};
-
-	bool addParticle(int x, int y, Particle* particle) {
-		/*Try to put a particle on the grid. Returns true if successful else false*/
-		if (matrix[x][y] == nullptr) {
-			matrix[x][y] = particle;
-			return true;
-		}
-		
-		return false;
-	}
-
-	bool canMoveHorizontally(int x, int y, int direction) {
-		/* Check if its possible to move one pixel in a given direction.
-		   1 means going right. -1 means going left.*/
-		if (direction == 1)
-			return matrix[x + direction][y] == nullptr && x < SCREEN_WIDTH;
-		else if (direction == -1)
-			return matrix[x + direction][y] == nullptr && x > 0;
-
-		return false;
-	}
-
-	bool applyVerticalForce(int x, int y) {
-		/*Apply gravity. Returns true if moved.*/
-		if (matrix[x][y + 1] == nullptr && y < SCREEN_HEIGHT - 1) {
-			matrix[x][y + 1] = matrix[x][y];
-			matrix[x][y] = nullptr;
-			(*matrix[x][y]).setSideMoves(0);
-			return true;
-		}
-
-		return false;
-	}
-
-	bool spread(int x, int y) {
-		/*Make a particle spread to one side or another. Returns true if moved.*/
-		const int movingDirection{ std::rand() % 2 == 0 ? -1 : 1 };
-		(*matrix[x][y]).setSideMoves(0);
-
-		if (canMoveHorizontally(x, y, movingDirection) && (*matrix[x][y]).getSideMoves() < (*matrix[x][y]).getMaxSideMoves()) {
-			matrix[x + movingDirection][y] = matrix[x][y];
-			matrix[x][y] = nullptr;
-
-			return true;
-		}
-
-		return false;
-	}
-};
 
 class Particle {
 private:
+	enum Element;
+	SDL_Rect body{};
 	SDL_Color color{};
+	Element type{};
 	int maxSideMoves{};
-	int sideMoves{};
-	int density{}; // To do: Particle of different density swap vertical positions accordingly.
-
 public:
-	enum Type {
-		SAND,
-		WATER,
-	};
-
-	Particle(Type type) {
+	Particle(int x, int y, Element type) {
+		body.x = x;
+		body.y = y;
 
 		switch (type) {
 		case SAND:
@@ -89,7 +30,6 @@ public:
 			color.b = 0x70;
 			color.a = 0xFF;
 			maxSideMoves = 8;
-			density = 3;
 			break;
 		case WATER:
 			color.r = 0x10;
@@ -97,35 +37,62 @@ public:
 			color.b = 0xF0;
 			color.a = 0xFF;
 			// To do: Solve this. A idea might be a particle of water move to the side when there is another particle of water above it. But that would need to improve the world.
-			maxSideMoves = 99999; 
-			density = 1;
+			maxSideMoves = 99999;
 			break;
 		}
 	}
 
-	void update() {
-		if (!applyVerticalForce()) {
-			spread();
-		}
-	}
+	enum Element {
+		SAND,
+		WATER
+	};
 
 	void draw(SDL_Renderer* renderer) {
 		SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-		SDL_RenderPoint(renderer, position.x, position.y);
+		SDL_RenderPoint(renderer, body.x, body.y);
 	}
 
-	void setSideMoves(int n) {
-		sideMoves = n;
-	}
-
-	int getSideMoves() const {
-		return sideMoves;
-	}
-
-	int getMaxSideMoves() const {
-		return maxSideMoves;
+	std::pair<int, int> getPosition() {
+		return { body.x, body.y };
 	}
 };
+
+
+class World {
+private:
+	Particle* matrix[10][100]{};
+public:
+	World() {};
+
+	bool addParticle(Particle* particle) {
+		/*Try to add a particle. Returns true if position was unoccupied.*/
+		auto position{ (*particle).getPosition() };
+
+		if (matrix[position.first][position.second] == nullptr) {
+			matrix[position.first][position.second] = particle;
+
+			return true;
+		}
+
+		return false;
+	}
+
+	void update() {};
+
+	void draw(SDL_Renderer* renderer) {
+		for (int h{}; h < SCREEN_HEIGHT; ++h) {
+			for (int x{}; x < SCREEN_WIDTH; ++x) {
+				Particle* currentParticle{ matrix[x][h] };
+
+				if (currentParticle != nullptr) {
+					(*currentParticle).draw(renderer);
+				}
+			}
+		}
+	}
+
+};
+
 
 int main(int argc, char* argv[]) {
 	// Global random seed
@@ -137,7 +104,7 @@ int main(int argc, char* argv[]) {
 	init(&window, &renderer, SCREEN_WIDTH, SCREEN_HEIGHT, SCALING);
 
 	// Main loop
-	std::vector<Particle> particles{};
+	World world{};
 	SDL_Event event{};
 	bool quit{ false };
 	float mouseX{};
@@ -148,7 +115,9 @@ int main(int argc, char* argv[]) {
 	while (quit == false) {
 		if (leftMouseButtonDown || rightMouseButtonDown) {
 			SDL_GetMouseState(&mouseX, &mouseY);
-			particles.push_back(Particle(static_cast<int>(mouseX / SCALING), static_cast<int>(mouseY / SCALING), leftMouseButtonDown ? Particle::SAND : Particle::WATER));
+			// Todo: Overflow here?
+			Particle* newParticle = new Particle(static_cast<int>(mouseX / SCALING), static_cast<int>(mouseY / SCALING), Particle::SAND);
+			//world.addParticle(newParticle);
 		}
 
 		while (SDL_PollEvent(&event)) {
@@ -183,10 +152,8 @@ int main(int argc, char* argv[]) {
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 
-		for (Particle& particle : particles) {
-			particle.update();
-			particle.draw(renderer);
-		}
+		// Render & update particles
+		//world.draw(renderer);
 
 		SDL_RenderPresent(renderer);
 
