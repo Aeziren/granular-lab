961504ee475603a545d4b1afca45ad557f84613b
Aeziren <miguelsl2000@gmail.com>
2025-05-31 21:05:04 +0100

    add water to click

--- granular-lab/granular-lab.cpp
+++ granular-lab/granular-lab.cpp
@@ -235,7 +235,7 @@ int main(int argc, char* argv[]) {
 	while (quit == false) {
 		if (leftMouseButtonDown || rightMouseButtonDown) {
 			SDL_GetMouseState(&mouseX, &mouseY);
-			Particle* newParticle = new Particle(static_cast<int>(mouseX / SCALING), static_cast<int>(mouseY / SCALING), Particle::SAND);
+			Particle* newParticle = new Particle(static_cast<int>(mouseX / SCALING), static_cast<int>(mouseY / SCALING), leftMouseButtonDown ? Particle::SAND : Particle::WATER);
 			world.addParticle(newParticle);
 		}
 
