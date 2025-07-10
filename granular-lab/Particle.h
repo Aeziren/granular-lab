3b01f099446a3cdf3b340cd0d27d4f5fd0ae5b4e
Aeziren <miguelsl2000@gmail.com>
2025-07-10 14:07:38 +0100

    add Doxygen comments

--- granular-lab/Particle.h
+++ granular-lab/Particle.h
@@ -2,27 +2,75 @@
 #include <SDL3/SDL.h>
 #include <utility>
 
+/*
+* class Particle
+* @brief Represents a particle type.
+*/
 class Particle {
 public:
 	enum Element {
 		SAND,
 		WATER
 	};
+
+	/*
+	* @brief Creates a particle.
+	* @param x, y: Particle position.
+	* @parm type: Particle element type.
+	*/
 	Particle(int x, int y, Element type);
+
+	/*
+	* @brief Draw the current particle.
+	* @param renderer: Pointer to the renderer.
+	*/
 	void draw(SDL_Renderer* renderer);
+
+	/*
+	* @brief Get the position of the current particle.
+	* @return (x, y) pair of the particle.
+	*/
 	std::pair<int, int> getPosition();
+
+	/*
+	* @brief Set the particle position.
+	* @param x, y: Position to be set.
+	*/
 	void setPosition(int x, int y);
+
+	/*
+	* @brief Reset the counter of side moves made by this particle.
+	*/
 	void resetSideMoves();
+
+	/*
+	* @brief Get the particle type.
+	* @return Element of the current particle.
+	*/
 	Element getElement();
+
+	/*
+	* @brief Get the particle density.
+	* @return Float with density of the current particle.
+	*/
 	float getDensity();
+
+	/*
+	* @brief Increases the counter of side moves made by this particle.
+	*/
 	void increaseSideMoves();
+
+	/*
+	* @brief Checks if the particle has exhausted its allowed side moves.
+	* @return True if the particle is out of moves, else false.
+	*/
 	bool outOfMoves();
 
 private:
-	SDL_Rect body{};
-	SDL_Color color{};
-	Element type{};
-	int maxSideMoves{};
-	int sideMoves{};
-	float density{}; // kg/m3 - for reference, air density = 1,2 | water = 1000 | sand = 1600 | steam = 0,6
+	SDL_Rect body{};      /**< Rectangle representing the particle's position and size. */
+	SDL_Color color{};    /**< Color of the particle. */
+	Element type{};       /**< Element type of the particle. */
+	int maxSideMoves{};   /**< Maximum allowed side moves for the particle. */
+	int sideMoves{};      /**< Current number of side moves made. */
+	float density{};      /**< Density of the particle in kg/m3. for reference, air density = 1,2 | water = 1000 | sand = 1600 | steam = 0,6 */
 };
--- granular-lab/World.h
+++ granular-lab/World.h
@@ -3,27 +3,85 @@
 #include <vector>
 #include "Particle.h"
 
+// Todo: Refactor so this class doesn't break the SRP.
 
+/*
+* @class World
+* @brief Represents the world where the particles lives and interacts with each other.
+* 
+* This class manages the matrix of particles, physics updates and rendering.
+*/
 class World {
 private:
 	using MatrixLine = std::vector<Particle*>;
-	std::vector<MatrixLine> matrix{};
 	enum MovingDirections {
 		UP,
 		DOWN,
 		LEFT,
 		RIGHT
 	};
+
+	std::vector<MatrixLine> matrix{}; /*< Vector that holds all particles. */
 	
+	/*
+	* @brief Apply vertical force to a particle falling or floating on air.
+	* @param particle: Pointer to a particle.
+	* @return true if particle moved, else false.
+	*/
 	bool applyVerticalForce(Particle* particle);
+
+	/*
+	* @brief Swap the position of 2 particles in the world.
+	* @param particle1, particle2: Pointer to the particle that will get swapped.
+	*/
 	void swapParticles(Particle* particle1, Particle* particle2);
+
+	/*
+	* @brief Chooses randomly a horizontal direction to go.
+	* @param particle: Pointer to the particle that will move horizontally.
+	*/
 	bool spread(Particle* particle);
+
+	/*
+	* @brief Get a particle and a direction and check if there is no 
+	* neighbor in that direction and its inbounds.
+	* @param particle: Pointer to the particle to check.
+	* @param direction: The direction to check if that particle can move.
+	* @return true if particle can move in given direction, else false.
+	*/
 	bool canMove(Particle* particle, MovingDirections direction);
+
+	/*
+	* @brief Get a buffer. Search on the grid for all active particles and saves them to buffer.
+	* @param bufferActiveParticles: Address of buffer that will store the active particles.
+	*/
 	void searchActiveParticles(std::vector<Particle*>& bufferActiveParticles);
 public:
+	/*
+	* @brief Setup the world matrix based on the SCREEN_WIDTH and SCREEN_HEIGHT constants.
+	*/
 	World();
+
+	/*
+	* @brief Deletes all particle in the current object.
+	*/
 	~World();
+
+	/*
+	* @brief Try to add a particle. 
+	* @param particle: Pointer to the particle to add.
+	* @return true if position was unoccupied, else false.
+	*/
 	bool addParticle(Particle* particle);
+
+	/*
+	* @brief Apply physics on all active particles on the world.
+	*/
 	void update();
+
+	/*
+	* @brief Get a renderer and uses it to draw all particles.
+	* @param renderer: Pointer to the renderer.
+	*/
 	void draw(SDL_Renderer* renderer);
 };
