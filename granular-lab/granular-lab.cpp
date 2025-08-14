54fd25bff09459992a52824a646deecc9f0c0b7e
Aeziren <miguelsl2000@gmail.com>
2025-08-14 11:06:35 +0100

    garantees that error is visible

--- granular-lab/granular-lab.cpp
+++ granular-lab/granular-lab.cpp
@@ -20,6 +20,8 @@ int main(int argc, char* argv[]) {
 	}
 	catch (const std::exception& ex) {
 		std::cout << "Error: " << ex.what() << '\n';
+		std::cout << "Press Enter to exit...";
+		std::cin.get();
 	}
 
 	// Close
