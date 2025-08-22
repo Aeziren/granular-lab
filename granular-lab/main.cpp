a4f8d3b5101adf342bb01ad58f6c81166bb05318
Aeziren <miguelsl2000@gmail.com>
2025-08-22 15:56:09 +0100

    rename to convention

---
+++ granular-lab/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+#include <SDL3/SDL.h>
+#include "helpers.h"
+#include "Constants.h"
+#include "playLoop.h"
+
+
+int main(int argc, char* argv[]) {
+	// Global random seed
+	std::srand(std::time(nullptr));
+
+	// Prepare
+	SDL_Window* window{};
+	SDL_Renderer* renderer{};
+	init(&window, &renderer, SCREEN_WIDTH, SCREEN_HEIGHT, SCALING);
+
+	try {
+		playLoop(window, renderer);
+	}
+	catch (const std::exception& ex) {
+		std::cout << "Error: " << ex.what() << '\n';
+		std::cout << "Press Enter to exit...";
+		std::cin.get();
+	}
+
+	// Close
+	close(&window, &renderer);
+	return 0;
+}
